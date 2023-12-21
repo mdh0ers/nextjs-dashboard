5e7833c47a487ad63edd51e2717d7c510985882b
Maxime Dhordain <maxime.dhordain@proton.me>
2023-12-21 18:41:30 +0100

    Chapter 4 ended

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import { lusitana } from "@/app/ui/fonts";
+
+export default function Page() {
+    return <p className={ `${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal` }>Invoices Page</p>;
+}
